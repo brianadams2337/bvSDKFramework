dd6403ced9fd332680b5030458c5f8219ef6fc52
Dezbird <desirae.beberniss@bazaarvoice.com>
2013-08-13 16:39:52 -0600

    Add conditional for Reward Zone fields

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
+// [dbeberniss] show Reward Zone Member Number field when Yes or Yes-PS value is selected
+$('body').on('change', '#contextdatavalue_rewardZoneMembershipV3ID', function() {
+    if (this.value == 'Yes') {
+        $('#additionalfield_RewardZoneNumberWrapperID').show();
+    } else if (this.value == 'Yes-PS') {
+        $('#additionalfield_RewardZoneNumberWrapperID').show();
+    } else {
+        $('#additionalfield_RewardZoneNumberWrapperID').hide();
+    }
+});
 
 // [dbeberniss] show tip boxes on click
-$('body').on('click','.BVPopinLauncher a', function(e) {
+$('body').on('click', '.BVPopinLauncher a', function(e) {
     $($(this).attr('href')).toggle();
     e.preventDefault();
 });
-
